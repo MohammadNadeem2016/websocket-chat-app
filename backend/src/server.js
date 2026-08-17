52c7fa640efca6fa685d1d3dbcea6a3f83ca0ce1
Mohammad Nadeem <mnadeem@thelcfgroup.com>
2026-08-17 14:27:42 +0530

    Boiler plate is ready as of now

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -10,8 +10,18 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () =>
   console.log("Chatify App is up and runing on PORT:", PORT),
